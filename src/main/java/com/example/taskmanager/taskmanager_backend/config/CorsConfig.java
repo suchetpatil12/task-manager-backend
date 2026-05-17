a5e4f104dd1defa3c02d85252da69f29911ea650
suchett12 <suchet.patil@muktainnovation.com>
2026-05-17 20:01:08 +0530

    Updated CORS for production frontend

--- src/main/java/com/example/taskmanager/taskmanager_backend/config/CorsConfig.java
+++ src/main/java/com/example/taskmanager/taskmanager_backend/config/CorsConfig.java
@@ -8,26 +8,65 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
+        CorsConfiguration configuration =
+                new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.setAllowedOrigins(
 
-        configuration.addAllowedMethod("*");
+                Arrays.asList(
 
-        configuration.addAllowedHeader("*");
+                        "http://localhost:4200",
+
+                        "https://task-manager-frontend-one-kappa.vercel.app"
+
+                )
+
+        );
+
+        configuration.setAllowedMethods(
+
+                Arrays.asList(
+
+                        "GET",
+
+                        "POST",
+
+                        "PUT",
+
+                        "DELETE",
+
+                        "OPTIONS"
+
+                )
+
+        );
+
+        configuration.setAllowedHeaders(
+
+                Arrays.asList("*")
+
+        );
 
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration(
+
+                "/**",
+
+                configuration
+
+        );
 
         return source;
     }
